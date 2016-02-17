130afb5e6af009859326398dbf868c52abf9710d
Sharfaz Sairaz <SharfazSairaz@se.local>
2016-02-17 14:33:41 +0000

    [] Initial symfont project for new salexadmin

---
+++ README.md
@@ -0,0 +1,4 @@
+newsalexadmin
+=============
+
+A Symfony project created on February 17, 2016, 12:17 pm.
---
+++ src/AppBundle/Controller/DefaultController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function indexAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('default/index.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
+        ]);
+    }
+    
+    /**
+     * @Route("lucky/number")
+     */
+    public function numberAction()
+    {
+    	$number = rand(0,100);
+    	
+    	return new Response(
+    		'<html><body>Lucky Number: '.$number .'</html>'
+    	);
+    }
+    
+    
+}
