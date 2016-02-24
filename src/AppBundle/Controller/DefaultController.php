48a0996efd61f395ddc9502ca94fecdac2ad949f
Mohamed Sharfaz Saira <sharfaz.sairaz@salary-exchange.co.uk>
2016-02-24 17:43:34 +0000

    Redirect index page to login and load new login page base layout in child templates

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -15,9 +15,10 @@ class DefaultController extends Controller
     public function indexAction(Request $request)
     {
         // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
+        /*return $this->render('default/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
-        ]);
+        ]);*/
+        return $this->redirectToRoute('show_dashboard');
     }
     
     /**
